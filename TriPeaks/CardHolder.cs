90ac47625497b83a1f222e87cbf2fddfd20f35cb
jhinder <jan@dfragment.net>
2015-02-21 04:19:34 +0100

    Better card managing algorithms (+ documentation)

--- TriPeaks/CardHolder.cs
+++ TriPeaks/CardHolder.cs
@@ -8,48 +8,132 @@ namespace TriPeaks
 {
     class CardHolder
     {
-
+        /// <summary>
+        /// The raw deck. Contains all available playing cards.
+        /// </summary>
         private static IList<Card> rawDeck;
 
-        private Stack<Card> bottomStack;
+        /// <summary>
+        /// The bottom stack from which the player can draw cards,
+        /// </summary>
+        public Stack<Card> BottomStack { get; set; }
 
-        private Card latestCard;
+        /// <summary>
+        /// The current card, on top of which all moves are made.
+        /// </summary>
+        public Card CurrentCard { get; set; }
 
+        /// <summary>
+        /// A list of all cards in use by the "pyramids".
+        /// </summary>
+        public List<Card> PyramidCards { get; set; }
+
+        /// <summary>
+        /// Static constructor; initiates the raw deck.
+        /// </summary>
         static CardHolder()
         {
             // Go through all cards once and add them to the base deck.
-            // This needs to be done exactly once per app run -> cctor.
             rawDeck = new List<Card>(GenerateDeck());
         }
 
+        /// <summary>
+        /// Sets the hidden status of the entire deck.
+        /// </summary>
+        /// <param name="hidden">Determines if the cards are supposed to be hidden (true) or shown (false).</param>
         private static void SetHiddenForDeck(bool hidden)
         {
             foreach (Card card in rawDeck)
                 card.Hidden = hidden;
         }
 
+        /// <summary>
+        /// Creates a new card holder/manager.
+        /// </summary>
         public CardHolder()
         {
-            // Place 23 cards in the bottom stack.
-            // (Yes, we get 24 cards at first. Hang on a second and you'll see why.)
-            int cardCount = 0;
+            SetHiddenForDeck(true);
+
+            // Shuffle the deck and place all cards in a queue.
+            var shuffledDeck = new Queue<Card>();
+            
             Random r = new Random();
             Card tmpCard;
-            do {
+            while (rawDeck.Any(c => c.Hidden))
+            {
                 int rand = r.Next(0, 51);
                 tmpCard = rawDeck.ElementAt(rand);
                 if (tmpCard.Hidden) {
+                    shuffledDeck.Enqueue(tmpCard);
                     rawDeck.ElementAt(rand).Hidden = false;
-                    bottomStack.Push(tmpCard);
-                    cardCount++;
                 }
-            } while (cardCount < 24);
+            }
+
+            // Place 23 cards in the bottom stack.
+            // (Yes, we get 24 cards at first. Hang on a second and you'll see why.)
+            for (int i = 0; i < 23; i++)
+                BottomStack.Push(shuffledDeck.Dequeue());
+            foreach (Card c in BottomStack)
+                c.Hidden = true;
 
             // Place one card as the initial playing card.
-            // For simplicity (no tracking of "burnt" card indexes) we get 24 random cards
-            latestCard = bottomStack.Pop();
+            CurrentCard = shuffledDeck.Dequeue();
+            CurrentCard.Hidden = false;
+
+            // The remaining 28 cards serve as the basis for the pyramid.
+            // ... 18 cards of which are hidden, 10 are shown.
+            var rehiddenCards = new Queue<Card>();
+            for (int i = 0; i < 18; i++) {
+                tmpCard = shuffledDeck.Dequeue();
+                tmpCard.Hidden = true;
+                rehiddenCards.Enqueue(tmpCard);
+            }
+            foreach (Card card in shuffledDeck)
+                rehiddenCards.Enqueue(card);
+
+            // And finally use the correctly turned queue as the basis for the pyramid cards.
+            PyramidCards = rehiddenCards.ToList();
+
         }
 
+        /// <summary>
+        /// Moves the top card of the bottom stack to the current playing card.
+        /// </summary>
+        public void MoveStackToCurrent()
+        {
+            if (BottomStack.Count() != 0)
+                CurrentCard = BottomStack.Pop();
+        }
+
+        /// <summary>
+        /// Tries to move a card from the pyramids to the current card.
+        /// </summary>
+        /// <param name="index">The index of the card within the list.</param>
+        /// <returns>Returns if the move was successful or allowed.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Is thrown when the index is not between 0 and 27.</exception>
+        public bool MovePyramidCardToCurrent(int index)
+        {
+            if (index < 0 || index >= 27)
+                throw new ArgumentOutOfRangeException("Pyramid index must be between 0 and 27");
+
+            var peekCard = PyramidCards.ElementAt(index);
+            if (peekCard.Hidden)
+                return false;
+            if (!CurrentCard.Value.IsAdjacentTo(peekCard.Value))
+                return false;
+
+            // It's okay to move the card
+            CurrentCard = peekCard;
+
+            //PyramidCards.ElementAt(index)
+            // TODO hide from view
+            return true;
+        }
+
+        /// <summary>
+        /// Generates a new deck.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerable"/> of <see cref="TriPeaks.Card"/>.</returns>
         private static IEnumerable<Card> GenerateDeck()
         {
             // Generation order: colours, then values
