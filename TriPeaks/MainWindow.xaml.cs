ed0282fb5e054f2c967432aa26ec3b169a98089d
jhinder <jan@dfragment.net>
2015-02-21 19:18:06 +0100

    Game window: almost all penalty confirmations

--- TriPeaks/MainWindow.xaml.cs
+++ TriPeaks/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace TriPeaks
     {
 
         private TriPeaksViewModel viewModel;
+        internal static bool GameInSession { get; private set; }
 
         public MainWindow()
         {
@@ -35,7 +36,7 @@ namespace TriPeaks
 
         private void DealCommandExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-
+            Redeal();
         }
 
         private async void ChangePlayerExecuted(object sender, ExecutedRoutedEventArgs e)
@@ -45,7 +46,13 @@ namespace TriPeaks
 
         private void ResetGameExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-
+            var mres = MessageBox.Show("Do you really want to reset your score to zero?",
+                "TriPeaks Reset",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question,
+                MessageBoxResult.Yes);
+            if (mres == MessageBoxResult.Yes)
+                ; // Reset statistics
         }
 
         private async void ChangeDeckExecuted(object sender, ExecutedRoutedEventArgs e)
@@ -73,6 +80,46 @@ namespace TriPeaks
         }
 
         #endregion
+
+        private void Redeal()
+        {
+            if (GameInSession) {
+                // Dealing while game in session = $140 penalty
+                var mres = MessageBox.Show("You are trying to deal with cards left in play for a penalty of 140 dollars. "
+                    + "Do you really want to redeal?",
+                    "TriPeaks Penalty",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question,
+                    MessageBoxResult.Yes);
+                if (mres == MessageBoxResult.No)
+                    return;
+                else
+                    ; // -$140
+            }
+            GameInSession = true;
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (GameInSession) {
+                var mres = MessageBox.Show("You are exiting with cards left in play for a penalty of 140 dollars."
+                    + " - OUCH!! Do you really want to quit?",
+                    "Exiting TiPreaks",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question,
+                    MessageBoxResult.Yes);
+
+                if (mres == MessageBoxResult.No)
+                    e.Cancel = true;
+                else
+                    ; // -$140
+            }
+        }
+
+        private void WindowCloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 
     internal class TriPeaksViewModel : INotifyPropertyChanged
