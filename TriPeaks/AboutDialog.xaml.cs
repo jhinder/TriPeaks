82f37c1bdba03257a0c5acce1f58df9262653d1a
jhinder <jan@dfragment.net>
2015-02-22 22:22:25 +0100

    Improved the About dialog

--- TriPeaks/AboutDialog.xaml.cs
+++ TriPeaks/AboutDialog.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Input;
 
 namespace TriPeaks
@@ -16,6 +18,21 @@ namespace TriPeaks
         private void CloseExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Window_SourceInitialized(object sender, System.EventArgs e)
+        {
+            IconHelper.RemoveIcon(this);
+        }
+
+        private async void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            await Dispatcher.InvokeAsync((Action)(() =>
+            {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+                e.Handled = true;
+            }));
+            
         }
     }
 }
